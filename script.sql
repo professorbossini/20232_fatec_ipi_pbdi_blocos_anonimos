9fce5b7a2acfcccf342ca57638370021cb67f772
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-05 09:09:16 -0300

    ap08(blocos-anonimos): codigo 2.4.1

--- script.sql
+++ script.sql
@@ -1,10 +1,28 @@
 DO
 $$
+DECLARE
+	codigo INTEGER := 1;
+	nome_completo VARCHAR(200) := 'João Santos';
+	-- 11 digitos no total, 2 para valores decimais
+	salario NUMERIC(11, 2) := 20.5;
 BEGIN
-	-- exibir uma mensagem
-	-- RAISE NOTICE 'Meu primeiro bloco anônimo';
-	-- placeholders (guardião de lugar)
-	-- 2 + 2 = 4
-	RAISE NOTICE '% + % = %', 2, 2, 2 + 2;	
+	-- fazer um raise notice que exibe o seguinte
+	--meu código é tal, me chamo tal e meu salário é tal
+	RAISE NOTICE 'Meu código é %, me chamo % e meu salário é %', codigo, nome_completo, salario;
 END;
 $$
+
+
+
+-- DO
+-- $$
+-- BEGIN
+-- 	-- exibir uma mensagem
+-- 	-- RAISE NOTICE 'Meu primeiro bloco anônimo';
+-- 	-- placeholders (guardião de lugar)
+-- 	-- 2 + 2 = 4
+-- 	RAISE NOTICE '% + % = %', 2, 2, 2 + 2;	
+-- END;
+-- $$
+
+
