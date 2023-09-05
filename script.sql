40812acd7fdbf39c487d6461aa826fbc90a8ed5f
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-05 08:55:08 -0300

    ap08(blocos-anonimos): codigo 2.3.1

---
+++ script.sql
@@ -0,0 +1,10 @@
+DO
+$$
+BEGIN
+	-- exibir uma mensagem
+	-- RAISE NOTICE 'Meu primeiro bloco anônimo';
+	-- placeholders (guardião de lugar)
+	-- 2 + 2 = 4
+	RAISE NOTICE '% + % = %', 2, 2, 2 + 2;	
+END;
+$$
